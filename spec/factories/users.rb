772db90b4d448d374979b60cec11213fa513713a
momom518 <edge_8_ryu@aol.com>
2020-07-05 19:06:50 +0900

    test

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {"田なか２"}
+    firstname             {"田中"}
+    lastname              {"その子"}
+    firstname_kana        {"タナカ"}
+    lastname_kana         {"ソノコ"}
+    email                 {"yyy@sample.com"}
+    password              {"111aaa"}
+    password_confirmation {"111aaa"}
+    birthday              {"1990-1-1"}
+  end
+end
