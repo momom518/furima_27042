cc1567a6f340946522bf8a809e2d4f78acd40d96
momom518 <edge_8_ryu@aol.com>
2020-07-05 19:10:43 +0900

    error_message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :firstname, :lastname, :firstname_kana, :lastname_kana, :password, :encrypted_password, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :firstname, :lastname, 
+      :firstname_kana, :lastname_kana, :password, :encrypted_password, :birthday])
   end
 
   # before_action :authenticate_user!
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,18 @@
 class User < ApplicationRecord
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable
+         :recoverable, :rememberable, :validatable
 
   with_options presence: true do
     validates :nickname
-    validates :email, uniqueness:true, length: { minimum: 6 },format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "＠を入れてください。"}
-    validates :firstname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
-    validates :lastname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
-    validates :firstname_kana, format: { with:/\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください。"}
-    validates :lastname_kana, format: { with:/\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください。"}
-    validates :password ,format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}\z/i, message: "６文字以上の英数字を入れてください。"}
-    validates :encrypted_password
+    validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :firstname, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :lastname, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :firstname_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :lastname_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}\z/i }
+    validates :password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}\z/i }
     validates :birthday
-  end      
+  end
 end
