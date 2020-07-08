0e019dc44e8346b0a22988abbc63a72a9c465b47
momom518 <edge_8_ryu@aol.com>
2020-07-08 18:11:40 +0900

    fix test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,80 +1,90 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  before do
+    @item = build(:item)
+  end
+
   it 'is valid with a name, image, information, category, condition, delivery, postage, prefecture, price' do
-    item = build(:item)
-    expect(item).to be_valid
+    expect(@item).to be_valid
   end
 
   it 'is invalid without name' do
-    item = build(:item, name: '')
-    item.valid?
-    expect(item.errors[:name]).to include("can't be blank")
+    @item.name = nil
+    @item.valid?
+    expect(@item.errors[:name]).to include("can't be blank")
   end
 
   it 'is invalid without image' do
-    item = build(:item, image: nil)
-    item.valid?
-    expect(item.errors[:image]).to include("can't be blank")
+    @item.image = nil
+    @item.valid?
+
+    expect(@item.errors[:image]).to include("can't be blank")
   end
 
   it 'is invalid without information' do
-    item = build(:item, information: '')
-    item.valid?
-    expect(item.errors[:information]).to include("can't be blank")
+    @item.information = nil
+    @item.valid?
+    expect(@item.errors[:information]).to include("can't be blank")
   end
 
   it 'is invalid without category' do
-    item = build(:item, category_id: '')
-    item.valid?
-    expect(item.errors[:category_id]).to include("can't be blank")
+    @item.category_id = nil
+    @item.valid?
+    expect(@item.errors[:category_id]).to include("can't be blank")
   end
 
   it 'is invalid without condition_id' do
-    item = build(:item, condition_id: '')
-    item.valid?
-    expect(item.errors[:condition_id]).to include("can't be blank")
+    @item.condition_id = nil
+    @item.valid?
+    expect(@item.errors[:condition_id]).to include("can't be blank")
   end
 
   it 'is invalid without delivery_id' do
-    item = build(:item, delivery_id: '')
-    item.valid?
-    expect(item.errors[:delivery_id]).to include("can't be blank")
+    @item.delivery_id = nil
+    @item.valid?
+    expect(@item.errors[:delivery_id]).to include("can't be blank")
   end
 
   it 'is invalid without postage_id' do
-    item = build(:item, postage_id: '')
-    item.valid?
-    expect(item.errors[:postage_id]).to include("can't be blank")
+    @item.postage_id = nil
+    @item.valid?
+    expect(@item.errors[:postage_id]).to include("can't be blank")
   end
 
   it 'is invalid without prefecture_id' do
-    item = build(:item, prefecture_id: '')
-    item.valid?
-    expect(item.errors[:prefecture_id]).to include("can't be blank")
+    @item.prefecture_id = nil
+    @item.valid?
+    expect(@item.errors[:prefecture_id]).to include("can't be blank")
   end
 
   it 'is invalid without price' do
-    item = build(:item, price: '')
-    item.valid?
-    expect(item.errors[:price]).to include("can't be blank")
+    @item.price = nil
+    @item.valid?
+    expect(@item.errors[:price]).to include("can't be blank")
   end
 
   it 'is invalid price that has more than 10000000' do
-    item = build(:item, price: '10000000')
-    item.valid?
-    expect(item.errors[:price]).to include('must be less than 9999999')
+    @item.price = 10_000_000
+    @item.valid?
+    expect(@item.errors[:price]).to include('must be less than 10000000')
   end
 
   it 'is invalid price that has less than 299' do
-    item = build(:item, price: '299')
-    item.valid?
-    expect(item.errors[:price]).to include('must be greater than or equal to 300')
+    @item.price = 299
+    @item.valid?
+    expect(@item.errors[:price]).to include('must be greater than or equal to 300')
   end
 
-  it 'is valid price that has more than ' do
-    item = build(:item, price: '9999999')
-    item.valid?
-    expect(item.errors[:price]).to include('must be less than 9999999')
+  it 'is valid price that has less than 9999999 ' do
+    @item.price = 9_999_999
+    @item.valid?
+    expect(@item).to be_valid
+  end
+
+  it 'is valid price that has more than 300 ' do
+    @item.price = 300
+    @item.valid?
+    expect(@item).to be_valid
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,133 +2,126 @@ require 'rails_helper'
 
 describe User do
   describe '#create' do
+    before do
+      @user = build(:user)
+    end
     it 'is valid with a nickname, firstname,lastname,firstname_kana,lastname_kana,email, password, password_confirmation' do
-      user = build(:user)
-      expect(user).to be_valid
+      expect(@user).to be_valid
     end
 
     it 'is invalid without a nickname' do
-      user = build(:user, nickname: '')
-      user.valid?
-      expect(user.errors[:nickname]).to include("can't be blank")
+      @user.nickname = nil
+      @user.valid?
+      expect(@user.errors[:nickname]).to include("can't be blank")
     end
 
     it 'is invalid without a email' do
-      user = build(:user, email: '')
-      user.valid?
-      expect(user.errors[:email]).to include("can't be blank")
+      @user.email = nil
+      @user.valid?
+      expect(@user.errors[:email]).to include("can't be blank")
     end
 
     it 'is invalid without firstname' do
-      user = build(:user, firstname: '')
-      user.valid?
-      expect(user.errors[:firstname]).to include("can't be blank")
+      @user.firstname = nil
+      @user.valid?
+      expect(@user.errors[:firstname]).to include("can't be blank")
     end
 
     it 'is invalid without lastname' do
-      user = build(:user, lastname: '')
-      user.valid?
-      expect(user.errors[:lastname]).to include("can't be blank")
+      @user.lastname = nil
+      @user.valid?
+      expect(@user.errors[:lastname]).to include("can't be blank")
     end
 
     it 'is invalid without firstname_kana' do
-      user = build(:user, firstname_kana: '')
-      user.valid?
-      expect(user.errors[:firstname_kana]).to include("can't be blank")
+      @user.firstname_kana = nil
+      @user.valid?
+      expect(@user.errors[:firstname_kana]).to include("can't be blank")
     end
 
     it 'is invalid without lastname_kana' do
-      user = build(:user, lastname_kana: '')
-      user.valid?
-      expect(user.errors[:lastname_kana]).to include("can't be blank")
+      @user.lastname_kana = nil
+      @user.valid?
+      expect(@user.errors[:lastname_kana]).to include("can't be blank")
     end
 
     it 'is invalid without password' do
-      user = build(:user, password: '')
-      user.valid?
-      expect(user.errors[:password]).to include("can't be blank")
+      @user.password = nil
+      @user.valid?
+      expect(@user.errors[:password]).to include("can't be blank")
     end
 
     it 'is invalid without password_confirmation' do
-      user = build(:user, password_confirmation: '')
-      user.valid?
-      expect(user.errors[:password_confirmation]).to include("can't be blank")
+      @user.password_confirmation = nil
+      @user.valid?
+      expect(@user.errors[:password_confirmation]).to include("can't be blank")
     end
 
     it 'is invalid without birthday ' do
-      user = build(:user, birthday: '')
-      user.valid?
-      expect(user.errors[:birthday]).to include("can't be blank")
+      @user.birthday = nil
+      @user.valid?
+      expect(@user.errors[:birthday]).to include("can't be blank")
     end
 
     it 'is invalid firstname hankaku ' do
-      user = build(:user, firstname: 'name1')
-      user.valid?
-      expect(user.errors[:firstname]).to include('is invalid')
+      @user.firstname = 'taro'
+      @user.valid?
+      expect(@user.errors[:firstname]).to include('is invalid')
     end
 
     it 'is invalid lastname hankaku ' do
-      user = build(:user, lastname: 'name1')
-      user.valid?
-      expect(user.errors[:lastname]).to include('is invalid')
+      @user.lastname = 'suzuki'
+      @user.valid?
+      expect(@user.errors[:lastname]).to include('is invalid')
     end
 
     it 'is invalid firstname_kana hankaku,zenkaku-kanji,zenkaku-hiragana ' do
-      user = build(:user, firstname_kana: 'name1')
-      user.valid?
-      expect(user.errors[:firstname_kana]).to include('is invalid')
+      @user.firstname_kana = '太朗'
+      @user.valid?
+      expect(@user.errors[:firstname_kana]).to include('is invalid')
     end
 
     it 'is invalid lastname_kana hankaku,zenkaku-kanji,zenkaku-hiragana ' do
-      user = build(:user, lastname_kana: 'name')
-      user.valid?
-      expect(user.errors[:lastname_kana]).to include('is invalid')
+      @user.lastname_kana = '鈴木'
+      @user.valid?
+      expect(@user.errors[:lastname_kana]).to include('is invalid')
     end
 
     it 'is invalid without a password_confirmation although with a password' do
-      user = build(:user, password_confirmation: 'fff1122')
-      user.valid?
-      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+      @user.password = 'fff1122'
+      @user.valid?
+      expect(@user.errors[:password_confirmation]).to include("doesn't match Password")
     end
 
     it 'is invalid with  that has less than 5 characters ' do
-      user = build(:user, password: '111aa')
-      user.valid?
-      expect(user.errors[:password]).to include('is invalid')
+      @user.password = '111aa'
+      @user.valid?
+      expect(@user.errors[:password]).to include('is invalid')
     end
 
     it 'is valid with that has more than 6 characters ' do
-      user = build(:user, password: '111aaa')
-      expect(user).to be_valid
+      @user.password = '111aaa'
+      expect(@user).to be_valid
     end
 
-    it 'is invalid with that has less than 5 characters ' do
-      user = build(:user, password_confirmation: '111aa')
-      user.valid?
-      expect(user.errors[:password_confirmation]).to include('is invalid')
+    it 'is invalid only number ' do
+      @user.password = '111111'
+      @user.valid?
+      expect(@user.errors[:password]).to include('is invalid')
     end
 
-    it 'is valid with that has more than 6 characters ' do
-      user = build(:user, password_confirmation: '111aaa')
-      expect(user).to be_valid
-    end
-    it 'is invalid both number and alphabet ' do
-      user = build(:user, password: '111111')
-      user.valid?
-      expect(user.errors[:password]).to include('is invalid')
+    it 'is invalid without @' do
+      @user.email = 'aaaaaa.com'
+      @user.valid?
+      expect(@user.errors[:email]).to include('is invalid')
     end
 
     it 'is invalid with a duplicate email address' do
       user = create(:user)
-      another_user = build(:user, email: user.email)
+      another_user = @user.dup
+      @user.save
       another_user.valid?
       expect(another_user.errors[:email]).to include('has already been taken')
-    end
-
-    it 'is invalid without @' do
-      user = build(:user, email: 'aaaaaaaaaa')
-      user.valid?
-      expect(user.errors[:email]).to include('is invalid')
     end
   end
 end
